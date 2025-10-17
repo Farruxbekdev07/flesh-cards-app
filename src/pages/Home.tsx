cb76fa5b455a0ebc003323230aaf3fa3bf26bfe6
Farrukhbek <farrukhbekmirzakulov07@gmail.com>
2025-10-17 19:42:07 +0500

    feat: setup branch

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,7 +4,7 @@ import Typography from "@mui/material/Typography";
 export default function Home() {
   return (
     <Box sx={{ display: "flex" }}>
-      <Typography variant="h2">Home</Typography>
+      <Typography variant="h2">Landing Page</Typography>
     </Box>
   );
 }
